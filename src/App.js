67d1c9215c0e8c6ca4627190bd5a9bc5e9124c2a
Flawless2506 <danielfabio2507@gmail.com>
2022-08-29 22:38:07 +0100

    VIVENDO E APRENDENDO

--- src/App.js
+++ src/App.js
@@ -1,25 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
 
-function App() {
+import Tasks from './components/Tasks'
+import"./App.css";
+
+
+const App = () => {
+ 
+ const [tasks, setTasks] = useState([
+  
+  {
+      id: '1',
+      title: 'Estudar programação',
+      completed: false,
+      
+  },
+  {
+      id: '2',
+      title: 'jogar computador',
+      completed: true,
+
+  },
+
+  {
+    id: '2',
+    title: 'jogar computador',
+    completed: true,
+
+},
+
+
+  
+  
+ ]);
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Meu primeiro brunch.
-          
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+  <>
+  <div className='container'>
 
+    <Tasks tasks={tasks}/>
+  </div>
+  
+  </>
+  );
+};
 export default App;
